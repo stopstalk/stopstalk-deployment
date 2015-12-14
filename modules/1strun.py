693f2eff35d5287fb84f9601958ecade14390841
raj454raj <raj454raj@gmail.com>
2015-12-14 19:42:55 +0530

    1strun gitignored

---
+++ modules/1strun.py
@@ -0,0 +1,11 @@
+from gluon import *
+# Configure mysql server
+current.mysql_user = "root"
+current.mysql_password = ""
+current.mysql_server = "localhost"
+current.mysql_dbname = "migration"
+
+# Configure mail options
+current.smtp_server = "smtp.gmail.com:587"
+current.sender_mail = "contactstopstalk@gmail.com"
+current.sender_password = "hulsbntklqnriskz"
