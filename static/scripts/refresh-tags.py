30a568e65a917f78a808fa7348a2be1c46326792
raj454raj <raj454raj@gmail.com>
2016-06-24 12:43:52 +0530

    Tag retrieval fix

--- static/scripts/refresh-tags.py
+++ static/scripts/refresh-tags.py
@@ -135,7 +135,7 @@ def get_tag(link, name):
 
     if row:
         prev_tags = row.tags
-        if (prev_tags != str(all_tags)) or \
+        if (prev_tags != str(all_tags)) and \
            (prev_tags == "[-]" and str(all_tags) != "[-]"):
             row.update_record(tags=str(all_tags),
                               problem_name=name,
