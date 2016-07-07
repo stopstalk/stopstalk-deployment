3948414bd905812b7539f34480922e253e2cc494
raj454raj <raj454raj@gmail.com>
2016-07-08 01:27:23 +0530

    HackerRank graph retrieval script added

---
+++ static/scripts/hackerrank_graph.py
@@ -0,0 +1,47 @@
+"""
+    Copyright (c) 2015-2016 Raj Patel(raj454raj@gmail.com), StopStalk
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+"""
+
+import requests
+import time
+
+handle = "tryingtocode"
+response = requests.get("https://www.hackerrank.com/rest/hackers/" + handle + "/rating_histories2")
+response = response.json()["models"]
+
+hackerrank_graphs = []
+for contest_class in response:
+    final_json = {}
+    for contest in contest_class["events"]:
+        time_stamp = contest["date"][:-5].split("T")
+        time_stamp = time.strptime(time_stamp[0] + " " + time_stamp[1],
+                                   "%Y-%m-%d %H:%M:%S")
+
+        final_json[time_stamp] = {"name": contest["contest_name"],
+                                  "url": contest["contest_slug"],
+                                  "rating": contest["rating"],
+                                  "rank": contest["rank"]}
+
+    hackerrank_graphs.append({"graph_name": "HackerRank - %s" % contest_class["category"],
+                              "graph_data": final_json})
+
+for graph in hackerrank_graphs:
+    print graph
