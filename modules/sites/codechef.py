2e884ea54dc857012d0b51c265c78943b58bdf1e
raj454raj <raj454raj@gmail.com>
2016-07-30 03:05:23 +0530

    Issue #85: CodeChef crawl from new endpoint

--- modules/sites/codechef.py
+++ modules/sites/codechef.py
@@ -21,6 +21,16 @@
 """
 
 from .init import *
+from urllib import urlencode
+
+PARAMS = {"page": 0,
+          "sort_by": "All",
+          "sorting_order": "asc",
+          "language": "All",
+          "status": "All",
+          "pcode": "",
+          "ccode": "",
+          "Submit": "GO"}
 
 class Profile(object):
     """
@@ -97,109 +107,138 @@ class Profile(object):
             return all_tags
 
     # -------------------------------------------------------------------------
-    def parallelize_codechef(self, handle, page, last_retrieved):
+    def process_trs(self, year, page, trs):
         """
-            Helper function for retrieving codechef submissions parallely
+            Process all the trs from the /submissions page of CodeChef
 
-            @param handle (String): CodeChef handle
-            @param page (Integer): Pagination page number
-            @param last_retrieved (DateTime): Last retrieved for that user
+            @param year (Number): year
+            @param page (Number): Pagination number
+            @param trs (List): List of trs from the corresponding page
         """
-
-        if self.retrieve_failed:
-            return
-
-        url = "https://www.codechef.com/recent/user?user_handle=" + \
-               handle + \
-               "&page=" + \
-               str(page)
-
-        tmp = get_request(url, headers={"User-Agent": user_agent})
-
-        # GET request failed
-        if tmp == -1 or tmp == {}:
-            self.retrieve_failed = True
-            return
-
-        d = ast.literal_eval(tmp.text)["content"]
 
         it = 1
-        self.submissions[handle][page] = {}
-        x = bs4.BeautifulSoup(d, "lxml")
-
-        trs = x.find_all("tr")[1:]
-        for i in trs:
-            self.submissions[handle][page][it] = []
-            submission = self.submissions[handle][page][it]
+        handle = self.handle
+        for tr in trs:
+            self.submissions[handle]["%d_%d" % (year, page)][it] = []
+            submission = self.submissions[handle]["%d_%d" % (year, page)][it]
+            it += 1
             append = submission.append
 
+            all_tds = tr.find_all("td")
+            if len(all_tds) == 1:
+                # No recent activity
+                continue
+
+            # Time of Submission
             try:
-                # tos = time_of_submission
-                tos = i.contents[0].contents[0]
+                time_stamp = self.parsetime(all_tds[1].contents[0])
             except AttributeError:
                 continue
-            tos = str(ast.literal_eval(repr(tos).replace("\\", "")))
-            tos = self.parsetime(tos)
-            curr = time.strptime(str(tos), "%Y-%m-%d %H:%M:%S")
 
+            curr = time.strptime(str(time_stamp), "%Y-%m-%d %H:%M:%S")
             # So cool optimization!
-            if curr <= last_retrieved:
-                return
+            if curr <= self.last_retrieved:
+                return "DONE"
+            # Do not retrieve any further because this leads to ambiguity
+            # If 2 hours ago => 2 hours 20 mins or 2 hours 14 mins ...
+            # Let the user come back later when the datetime is exact
+            # This prevents from redundant addition into database
+            # @ToDo: For now we are allowing redundant submissions
+            #        for codechef :/ . Find a way to change it.
+            #if str(time_stamp).__contains__("hours"):
+            #   continue
+            append(str(time_stamp))
 
-            append(str(tos))
-
-            # Problem name/url
-            prob = i.contents[1].contents[0]
-            prob["href"] = "http://www.codechef.com" + prob["href"]
-            problem_link = eval(repr(prob["href"]).replace("\\", ""))
+            # Problem name/URL
+            problem_link = "https://www.codechef.com" + \
+                            all_tds[3].contents[0]["href"]
             append(problem_link)
-            try:
-                append(prob.contents[0])
-            except IndexError:
-                append("")
+            problem_name = all_tds[3].contents[0].contents[0]
+            append(problem_name)
 
             # Submission status
-            status = i.contents[2].contents[0].find("img")["src"]
-            status = repr(status).replace("\\", "")
-            status = status[7:-5]
+            status_span = all_tds[5].find("span")
+            status = ""
+            if status_span["title"] == "":
+                status = status_span.text
+            else:
+                status = status_span["title"]
+
             submission_status = "AC"
-            if status == "tick-icon":
+            points = "0"
+            if status.__contains__("pts"):
                 submission_status = "AC"
-            elif status == "cross-icon":
+                points = re.sub("\[.*?\]", "", status)
+            elif status == "accepted":
+                points = "100"
+                submission_status = "AC"
+            elif status == "wrong answer":
                 submission_status = "WA"
-            elif status == "alert-icon":
+            elif status == "compilation error":
                 submission_status = "CE"
-            elif status == "runtime-error":
+            elif status == "runtime error(SIGSEGV)":
                 submission_status = "RE"
-            elif status == "clock_error":
+            elif status == "time limit exceeded":
                 submission_status = "TLE"
             else:
                 submission_status = "OTH"
             append(submission_status)
 
-            # Question points
-            pts = i.contents[2].contents[0].contents
-            try:
-                if  len(pts) >= 5:
-                    points = pts[2] + " " + pts[4]
-                else:
-                    points = pts[2]
-            except IndexError:
-                if submission_status == "AC":
-                    points = "100"
-                else:
-                    points = "0"
+            # Submission points
             append(points)
 
-            # Language
-            append(i.contents[3].contents[0].strip())
+            # Submission language
+            language = all_tds[8].contents[0]
+            append(language)
 
-            # View code link
-            # @ToDo: Find a way to get the code link
-            view_link = ""
+            # View link
+            try:
+                view_link = "https://www.codechef.com" + all_tds[9].find("a")["href"]
+            except KeyError:
+                view_link = ""
             append(view_link)
 
-            it += 1
+    # -------------------------------------------------------------------------
+    def process_page(self, year, page, url):
+        """
+            Process a particular submissions page
+
+            @param year (Number): year
+            @param page (Number): Pagination number
+            @param url (String): url of the page to be processed
+        """
+        if self.retrieve_failed:
+            # Note: Any thread can fail
+            return
+
+        response = get_request(url, headers={"User-Agent": user_agent})
+        if response == -1 or response == {}:
+            self.retrieve_failed = True
+            return
+
+        soup = bs4.BeautifulSoup(response.text, "lxml")
+        trs = soup.find("div", class_="tablebox").find("tbody").find_all("tr")
+        self.process_trs(year, page, trs)
+
+    # -------------------------------------------------------------------------
+    def process_parallely(self, year, last_page):
+        """
+            Process various pages of a particular year parallely
+
+            @param year (Number): year
+            @param last_page (Number): Last page number
+        """
+
+        threads = []
+        params = dict(PARAMS)
+        params["year"] = year
+        for page in xrange(1, last_page):
+            params["page"] = page
+            self.submissions[self.handle]["%d_%d" % (year, page)] = {}
+            url = "https://www.codechef.com/submissions?" + urlencode(params)
+            threads.append(gevent.spawn(self.process_page, year, page, url))
+
+        gevent.joinall(threads)
 
     # -------------------------------------------------------------------------
     def get_submissions(self, last_retrieved):
@@ -210,136 +249,34 @@ class Profile(object):
             @return (Dict): Dictionary of submissions containing all the
                             information about the submissions
         """
-
-        if self.retrieve_failed:
-            return -1
-
         if self.handle:
             handle = self.handle
         else:
             return -1
 
-        user_url = "http://www.codechef.com/recent/user?user_handle=" + handle
+        self.last_retrieved = last_retrieved
+        PARAMS["handle"] = handle
+        start_year = int(current.INITIAL_DATE.split("-")[0])
+        current_year = datetime.datetime.now().year
+        for year in xrange(current_year, start_year - 1, -1):
+            params = dict(PARAMS)
+            params["year"] = year
+            url = "https://www.codechef.com/submissions?" + urlencode(params)
+            response = get_request(url,
+                                   headers={"User-Agent": user_agent})
+            soup = bs4.BeautifulSoup(response.text, "lxml")
+            trs = soup.find("div", class_="tablebox").find("tbody").find_all("tr")
 
-        tmp = get_request(user_url, headers={"User-Agent": user_agent})
+            self.submissions[handle]["%d_%d" % (year, 0)] = {}
+            ret = self.process_trs(year, 0, trs)
+            if ret:
+                return self.submissions
 
-        if tmp == -1 or tmp == {}:
-            return tmp
+            pagination = soup.find("div", class_="pageinfo")
+            if pagination:
+                last_page = pagination.contents[0].split(" of ")[1]
+                self.process_parallely(year, int(last_page))
 
-        d = ast.literal_eval(tmp.text)
-        max_page = d["max_page"]
-        submissions = {handle: {}}
-        it = 1
-
-        # Apply parallel processing only if retrieving from the INITIAL_DATE
-        tmp_const = time.strptime(current.INITIAL_DATE, "%Y-%m-%d %H:%M:%S")
-        if tmp_const == last_retrieved:
-            threads = []
-            for i in xrange(max_page):
-                threads.append(gevent.spawn(self.parallelize_codechef,
-                                            handle,
-                                            i,
-                                            last_retrieved))
-            gevent.joinall(threads)
-            return self.submissions
-
-        else:
-            for page in xrange(0, max_page):
-                user_url = "http://www.codechef.com/recent/user?user_handle=" + \
-                           handle + \
-                           "&page=" + \
-                           str(page)
-
-                tmp = get_request(user_url, headers={"User-Agent": user_agent})
-
-                if tmp == -1:
-                    return -1
-
-                d = ast.literal_eval(tmp.text)["content"]
-
-                submissions[handle][page] = {}
-                x = bs4.BeautifulSoup(d, "lxml")
-                trs = x.find_all("tr")[1:]
-                for i in trs:
-
-                    submissions[handle][page][it] = []
-                    submission = submissions[handle][page][it]
-                    append = submission.append
-
-                    try:
-                        # tos = time_of_submission
-                        tos = i.contents[0].contents[0]
-                    except AttributeError:
-                        continue
-                    tos = str(ast.literal_eval(repr(tos).replace("\\", "")))
-
-                    # Do not retrieve any further because this leads to ambiguity
-                    # If 2 hours ago => 2 hours 20 mins or 2 hours 14 mins ...
-                    # Let the user come back later when the datetime is exact
-                    # This prevents from redundant addition into database
-                    # @ToDo: For now we are allowing redundant submissions
-                    #        for codechef :/ . Find a way to change it.
-                    #if tos.__contains__("hours"):
-                    #   continue
-
-                    tos = self.parsetime(tos)
-                    curr = time.strptime(str(tos), "%Y-%m-%d %H:%M:%S")
-
-                    if curr <= last_retrieved:
-                        return submissions
-                    append(str(tos))
-
-                    # Problem name/url
-                    prob = i.contents[1].contents[0]
-                    prob["href"] = "http://www.codechef.com" + prob["href"]
-                    problem_link = eval(repr(prob["href"]).replace("\\", ""))
-                    append(problem_link)
-                    try:
-                        append(prob.contents[0])
-                    except IndexError:
-                        append("")
-
-                    # Submission status
-                    status = i.contents[2].contents[0].find("img")["src"]
-                    status = repr(status).replace("\\", "")
-                    status = status[7:-5]
-                    submission_status = "AC"
-                    if status == "tick-icon":
-                        submission_status = "AC"
-                    elif status == "cross-icon":
-                        submission_status = "WA"
-                    elif status == "alert-icon":
-                        submission_status = "CE"
-                    elif status == "runtime-error":
-                        submission_status = "RE"
-                    elif status == "clock_error":
-                        submission_status = "TLE"
-                    else:
-                        submission_status = "OTH"
-                    append(submission_status)
-
-                    # Question points
-                    pts = i.contents[2].contents[0].contents
-                    try:
-                        if len(pts) >= 5:
-                            points = pts[2] + " " + pts[4]
-                        else:
-                            points = pts[2]
-                    except IndexError:
-                        if submission_status == "AC":
-                            points = "100"
-                        else:
-                            points = "0"
-                    append(points)
-
-                    # Language
-                    append(i.contents[3].contents[0].strip())
-
-                    # View code link
-                    view_link = ""
-                    append(view_link)
-
-                    it += 1
-            return submissions
+        return self.submissions
 
 # =============================================================================
