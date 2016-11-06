e930a92d60166df84620ae6faf5a144e0010fc74
raj454raj <raj454raj@gmail.com>
2016-11-07 00:57:26 +0530

    [Issue #98]: default/faq.js added

---
+++ static/js/appjs/default/faq.js
@@ -0,0 +1,9 @@
+(function($) {
+    "use strict";
+
+    $(document).ready(function() {
+        $('.collapsible').collapsible({
+            accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+        });
+    });
+})(jQuery);
