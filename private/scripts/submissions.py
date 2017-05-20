dce5895f9f4ee20e05248bc139ee65a4bb82800d
raj454raj <raj454raj@gmail.com>
2017-05-21 00:14:31 +0530

    CodeChef retrieval paused. Courtesy: Snackdown

--- private/scripts/submissions.py
+++ private/scripts/submissions.py
@@ -303,6 +303,9 @@ def retrieve_submissions(record, custom, all_sites=current.SITES):
     global INVALID_HANDLES
     global failed_user_retrievals
 
+    if "CodeChef" in all_sites:
+        all_sites.remove("CodeChef")
+
     time_conversion = "%Y-%m-%d %H:%M:%S"
     list_of_submissions = []
     retrieval_failures = []
