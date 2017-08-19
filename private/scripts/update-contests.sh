ccc5d7d97ac2f71d17aaffb22ad687c72e5b6db6
raj454raj <raj454raj@gmail.com>
2017-08-20 02:19:50 +0530

    Cron script to regularly update contest data

---
+++ private/scripts/update-contests.sh
@@ -0,0 +1,24 @@
+total_iterations=50
+
+# Edit this according to your local environment
+web2py_directory="/home/www-data/web2py/"
+
+web2py_file="web2py.py"
+web2py_file=$web2py_directory$web2py_file
+
+script_file="applications/stopstalk/private/scripts/update-graph-data.py"
+script_file=$web2py_directory$script_file
+
+python_path=`which python`
+
+for i in $(seq 0 $[$total_iterations - 1])
+do
+    cmd="$python_path $web2py_file -S stopstalk -M -R $script_file -A codechef,codeforces,hackerrank batch $i $total_iterations"
+    echo $cmd
+    eval $cmd
+
+    if [ $i != $[$total_iterations - 1] ]
+    then
+        sleep 2
+    fi
+done
