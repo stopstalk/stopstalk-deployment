a946fdc0d410dde8e19ff92102776e0242068a67
raj454raj <raj454raj@gmail.com>
2018-07-08 01:32:26 +0530

    codeforces gymProblem fix

--- modules/sites/codeforces.py
+++ modules/sites/codeforces.py
@@ -139,13 +139,13 @@ class Profile(object):
             curr = time.gmtime(row["creationTimeSeconds"] + 330 * 60)
             if curr <= last_retrieved:
                 return submissions
- 
+
             if row.has_key("contestId") == False:
                 print "Contest ID not found for", row["problem"]["name"]
                 continue
 
             arg = "problem/"
-            if len(str(row["contestId"])) > 3:
+            if int(row["contestId"]) > 90000:
                 arg = "gymProblem/"
 
             # Problem Name/URL
