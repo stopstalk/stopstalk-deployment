dc041db8727f24e70eb8ed3d49255d4a2aa2dacf
raj454raj <raj454raj@gmail.com>
2018-09-02 00:21:55 +0530

    add analytics for input fields

--- static/js/appjs/google_analytics.js
+++ static/js/appjs/google_analytics.js
@@ -10,12 +10,13 @@
         });
     };
 
-    var addEventListener = function(selector, label, buttonLabel) {
-        $(document).on('click', selector, function() {
+    var addEventListener = function(selector, label, buttonLabel, eventType) {
+        if (!eventType) eventType = 'click';
+        $(document).on(eventType, selector, function() {
             if (buttonLabel) {
-                sendToGA('button', $(this).data('analytics-label'), 'click');
+                sendToGA('button', $(this).data('analytics-label'), eventType);
             } else {
-                sendToGA('button', label, 'click');
+                sendToGA('button', label, eventType);
             }
         });
         if (buttonLabel) {
@@ -28,115 +29,121 @@
     var addNavItemsToGA = function() {
         addEventListener('.side-nav-item', '', true);
         addEventListener('.nav-dropdown', '', true);
-        addEventListener('#help-us-continue', 'Donate');
+        addEventListener('#help-us-continue', 'Donate', false);
     };
 
     var addSubmissionPageButtonsToGA = function() {
-        addEventListener('.download-submission-button', 'Submission Download');
-        addEventListener('#final-download-button', 'Download button from Modal');
-        addEventListener('.view-submission-button', 'View submission button');
-        addEventListener('#copy-to-clipboard', 'Copy to Clipboard');
-        addEventListener('#final-view-download-button', 'Download button View Modal');
-        addEventListener('#link-to-site', 'View submission on Site');
-        addEventListener('.problem-listing', 'Problem in widget');
-        addEventListener('.add-to-todo-list', 'Add to todo list');
-        addEventListener('.submission-user-name', 'User\'s name on submission page');
-        addEventListener('.submission-site-profile', 'User\'s site profile on submission page');
+        addEventListener('.download-submission-button', 'Submission Download', false);
+        addEventListener('#final-download-button', 'Download button from Modal', false);
+        addEventListener('.view-submission-button', 'View submission button', false);
+        addEventListener('#copy-to-clipboard', 'Copy to Clipboard', false);
+        addEventListener('#final-view-download-button', 'Download button View Modal', false);
+        addEventListener('#link-to-site', 'View submission on Site', false);
+        addEventListener('.problem-listing', 'Problem in widget', false);
+        addEventListener('.add-to-todo-list', 'Add to todo list', false);
+        addEventListener('.submission-user-name', 'User\'s name on submission page', false);
+        addEventListener('.submission-site-profile', 'User\'s site profile on submission page', false);
     };
 
     var addProfilePageButtonsToGA = function() {
-        addEventListener('#profile-institute-leaderboard', 'Profile Institute Leaderboard')
-        addEventListener('#profile-user-submissions', 'Profile User Submissions');
-        addEventListener('#profile-add-friend', 'Profile Add Friend');
-        addEventListener('#profile-add-to-my-custom-friend', 'Profile Add to my Custom Friend');
-        addEventListener('#profile-add-to-my-custom-friend-logged-out', 'Profile Add to my Custom Friend (Logged out)');
-        addEventListener('#profile-unfriend', 'Profile Unfriend');
-        addEventListener('#profile-custom-user-of', 'Profile Custom User of');
+        addEventListener('#profile-institute-leaderboard', 'Profile Institute Leaderboard', false);
+        addEventListener('#profile-user-submissions', 'Profile User Submissions', false);
+        addEventListener('#profile-add-friend', 'Profile Add Friend', false);
+        addEventListener('#profile-add-to-my-custom-friend', 'Profile Add to my Custom Friend', false);
+        addEventListener('#profile-add-to-my-custom-friend-logged-out', 'Profile Add to my Custom Friend (Logged out)', false);
+        addEventListener('#profile-unfriend', 'Profile Unfriend', false);
+        addEventListener('#profile-custom-user-of', 'Profile Custom User of', false);
         // @ToDo: Generalize this
-        addEventListener('#codechef-profile-url', 'Codechef profile URL');
-        addEventListener('#codeforces-profile-url', 'CodeForces profile URL');
-        addEventListener('#hackerrank-profile-url', 'HackerRank profile URL');
-        addEventListener('#hackerearth-profile-url', 'HackerEarth profile URL');
-        addEventListener('#spoj-profile-url', 'Spoj profile URL');
-        addEventListener('#uva-profile-url', 'UVa profile URL');
-        addEventListener('#toggle-submission-graph', 'Toggle Submission Graph');
-        addEventListener('.popup-contest-page', 'Contest Page button on User Profile');
-        addEventListener('.custom-user-count-profile-page', 'Custom user count on User Profile');
-        addEventListener('#update-my-submissions', 'Refresh my submissions');
-        addEventListener('#disabled-update-my-submissions', 'Disabled refresh my submissions');
+        addEventListener('#codechef-profile-url', 'Codechef profile URL', false);
+        addEventListener('#codeforces-profile-url', 'CodeForces profile URL', false);
+        addEventListener('#hackerrank-profile-url', 'HackerRank profile URL', false);
+        addEventListener('#hackerearth-profile-url', 'HackerEarth profile URL', false);
+        addEventListener('#spoj-profile-url', 'Spoj profile URL', false);
+        addEventListener('#uva-profile-url', 'UVa profile URL', false);
+        addEventListener('#toggle-submission-graph', 'Toggle Submission Graph', false);
+        addEventListener('.popup-contest-page', 'Contest Page button on User Profile', false);
+        addEventListener('.custom-user-count-profile-page', 'Custom user count on User Profile', false);
+        addEventListener('#update-my-submissions', 'Refresh my submissions', false);
+        addEventListener('#disabled-update-my-submissions', 'Disabled refresh my submissions', false);
     };
 
     var addMiscellaneousToGA = function() {
-        addEventListener('#heart-button', 'Heart Button');
-        addEventListener('#heart-facebook-group', 'Heart Facebook Group');
-        addEventListener('#heart-facebook', 'Heart Facebook');
-        addEventListener('#heart-twitter', 'Heart Twitter');
-        addEventListener('#heart-google-plus', 'Heart Google Plus');
-        addEventListener('#heart-github', 'Heart Github');
-        addEventListener('#footer-faqs', 'Footer FAQs');
-        addEventListener('#footer-contact-us', 'Footer Contact Us');
-        addEventListener('#footer-license', 'Footer MIT License');
-        addEventListener('#footer-contributors', 'Footer Contributors');
-        addEventListener('#footer-raj454raj', 'Footer raj454raj');
-        addEventListener('#first-friend-search', 'First friend search');
-        addEventListener('#first-custom-friend', 'First custom friend');
-        addEventListener('.remove-from-todo', 'Remove from todo');
-        addEventListener('#testimonials-page', 'Write a Testimonial');
-        addEventListener('.custom-user-count', 'Open custom user list');
-        addEventListener('.custom-user-list-name', 'Custom user name in Modal');
-        addEventListener('.custom-user-modal-site-profile', 'Custom user Site Profile in Modal');
-        addEventListener('#open-side-nav', 'Open Side Navbar');
+        addEventListener('#heart-button', 'Heart Button', false);
+        addEventListener('#heart-facebook-group', 'Heart Facebook Group', false);
+        addEventListener('#heart-facebook', 'Heart Facebook', false);
+        addEventListener('#heart-twitter', 'Heart Twitter', false);
+        addEventListener('#heart-google-plus', 'Heart Google Plus', false);
+        addEventListener('#heart-github', 'Heart Github', false);
+        addEventListener('#footer-faqs', 'Footer FAQs', false);
+        addEventListener('#footer-contact-us', 'Footer Contact Us', false);
+        addEventListener('#footer-license', 'Footer MIT License', false);
+        addEventListener('#footer-contributors', 'Footer Contributors', false);
+        addEventListener('#footer-raj454raj', 'Footer raj454raj', false);
+        addEventListener('#first-friend-search', 'First friend search', false);
+        addEventListener('#first-custom-friend', 'First custom friend', false);
+        addEventListener('.remove-from-todo', 'Remove from todo', false);
+        addEventListener('#testimonials-page', 'Write a Testimonial', false);
+        addEventListener('.custom-user-count', 'Open custom user list', false);
+        addEventListener('.custom-user-list-name', 'Custom user name in Modal', false);
+        addEventListener('.custom-user-modal-site-profile', 'Custom user Site Profile in Modal', false);
+        addEventListener('#open-side-nav', 'Open Side Navbar', false);
     };
 
     var addProblemPageButtonsToGA = function() {
-        addEventListener('#my-submissions-tab', 'My Submissions Tab');
-        addEventListener('#friends-submissions-tab', 'Friends Submissions Tab');
-        addEventListener('#global-submissions-tab', 'Global Submissions Tab');
-        addEventListener('.problem-page-site-link', 'Problem page problem link');
-        addEventListener('.problem-page-editorials', 'Problem page editorials link');
-        addEventListener('.suggest-tags-plus-logged-out', 'Suggest tags plus (Logged out)');
-        addEventListener('.suggest-tags-plus', 'Suggest tags plus');
-        addEventListener('#show-tags', 'Show tags');
-        addEventListener('.problem-page-tag', 'Problem page tag');
+        addEventListener('#my-submissions-tab', 'My Submissions Tab', false);
+        addEventListener('#friends-submissions-tab', 'Friends Submissions Tab', false);
+        addEventListener('#global-submissions-tab', 'Global Submissions Tab', false);
+        addEventListener('.problem-page-site-link', 'Problem page problem link', false);
+        addEventListener('.problem-page-editorials', 'Problem page editorials link', false);
+        addEventListener('.suggest-tags-plus-logged-out', 'Suggest tags plus (Logged out)', false);
+        addEventListener('.suggest-tags-plus', 'Suggest tags plus', false);
+        addEventListener('#show-tags', 'Show tags', false);
+        addEventListener('.problem-page-tag', 'Problem page tag', false);
     };
 
     var addUserEditorialsButtonsToGA = function() {
-        addEventListener('#all-editorials-button', 'Read editorial page All Editorials');
-        addEventListener('#site-editorial-link', 'Site editorial link');
-        addEventListener('#show-editor', 'Write Editorial button');
-        addEventListener('#submit-editorial', 'Submit Editorial button');
-        addEventListener('#cancel-editorial', 'Cancel Editorial button');
+        addEventListener('#all-editorials-button', 'Read editorial page All Editorials', false);
+        addEventListener('#site-editorial-link', 'Site editorial link', false);
+        addEventListener('#show-editor', 'Write Editorial button', false);
+        addEventListener('#submit-editorial', 'Submit Editorial button', false);
+        addEventListener('#cancel-editorial', 'Cancel Editorial button', false);
     };
 
     var addFriendsPageButtonsToGA = function() {
-        addEventListener('.friends-name', 'Friends name');
-        addEventListener('.friends-unfriend', 'Friends unfriend');
-        addEventListener('.friends-add-friend', 'Friends add friend');
+        addEventListener('.friends-name', 'Friends name', false);
+        addEventListener('.friends-unfriend', 'Friends unfriend', false);
+        addEventListener('.friends-add-friend', 'Friends add friend', false);
     };
 
     var addSearchPageButtonsToGA = function() {
-        addEventListener('.search-add-friend', 'Search add friend');
-        addEventListener('.search-unfriend', 'Search unfriend');
-        addEventListener('.search-site-profile', 'Search site profile');
-        addEventListener('.search-user-name', 'Search user name');
+        addEventListener('.search-add-friend', 'Search add friend', false);
+        addEventListener('.search-unfriend', 'Search unfriend', false);
+        addEventListener('.search-site-profile', 'Search site profile', false);
+        addEventListener('.search-user-name', 'Search user name', false);
     };
 
     var addContestPageButtonsToGA = function() {
-        addEventListener('.view-contest', 'View Contest');
-        addEventListener('.set-reminder', 'Set reminder');
+        addEventListener('.view-contest', 'View Contest', false);
+        addEventListener('.set-reminder', 'Set reminder', false);
 
     };
 
     var addLeaderboardPageButtonsToGA = function() {
-        addEventListener('.leaderboard-institute', 'Leaderboard Institute');
-        addEventListener('.leaderboard-country-flag', 'Leaderboard Country');
-        addEventListener('.leaderboard-stopstalk-handle', 'Leaderboard StopStalk handle');
-        addEventListener('#leaderboard-switch', 'Leaderboard Switch');
+        addEventListener('.leaderboard-institute', 'Leaderboard Institute', false);
+        addEventListener('.leaderboard-country-flag', 'Leaderboard Country', false);
+        addEventListener('.leaderboard-stopstalk-handle', 'Leaderboard StopStalk handle', false);
+        addEventListener('#leaderboard-switch', 'Leaderboard Switch', false);
     };
 
-    var addTagsPageButtonsToGA = function() {
-        addEventListener('.tag-problem-link', 'Tags problem link');
-        addEventListener('.tags-chip', 'Tags page chip');
+    var addProblemSearchPageToGA = function() {
+        addEventListener('.tag-problem-link', 'Tags problem link', false);
+        addEventListener('.tags-chip', 'Tags page chip', false);
+        addEventListener('#problem-name', 'Problem Search Problem name field', false);
+        addEventListener('#profile-site', 'Problem Search Site field', false, 'change');
+        addEventListener('#orderby-problem', 'Problem Search Order By field', false, 'change');
+        addEventListener('#search', 'Problem Search Site Tag field', false);
+        addEventListener('#generalized-tag-search', 'Problem Search Generalized Tag field', false, 'change');
+        addEventListener('#include-editorials', 'Problem Search Include Editorials field', false);
     };
 
     $(document).ready(function() {
@@ -148,7 +155,7 @@
         addSearchPageButtonsToGA();
         addContestPageButtonsToGA();
         addLeaderboardPageButtonsToGA();
-        addTagsPageButtonsToGA();
+        addProblemSearchPageToGA();
         addMiscellaneousToGA();
     });
 })(jQuery);
