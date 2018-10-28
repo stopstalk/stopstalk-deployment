4bd04ad285af4a60cc292aa38077b965859eb8c5
raj454raj <raj454raj@gmail.com>
2018-10-28 18:49:13 +0530

    newsletter for latest updates

--- private/scripts/send-mail.py
+++ private/scripts/send-mail.py
@@ -27,25 +27,26 @@ query = (atable.registration_key == "") & \
 rows = db(query).select(atable.stopstalk_handle, atable.email)
 
 for row in rows:
-    subject = "UVa Online Judge added on StopStalk"
+    subject = "Recent enhancements on StopStalk"
     message = """
 <html>
 Hello %s,<br/><br/>
 
-As many of you have requested us to add UVa to StopStalk - here we are with the announcement
-that we have successfully added <b>UVa Online Judge</b> to StopStalk.
-To update your StopStalk profile with UVa submissions - Add your UVa username / handle <a href="https://www.stopstalk.com/user/update_details">here</a>. <br/>
-For your custom users you can update the handles <a href="https://www.stopstalk.com/user/custom_friend">here</a><br/>
-Climb the StopStalk leaderboard now ;) <br/><br/>
-
-Also, its been a while since we have conducted a survey to understand your use-cases better and incorporate them to improve StopStalk. <br/>
-Please take some minutes to fill out the <a href="https://goo.gl/oeLZHd">Survey Form</a><br/><br/>
-Adjust your email preferences <a href="https://www.stopstalk.com/unsubscribe">here</a><br/><br/>
+We have a few major announcements to make your experience on StopStalk better - <br/>
+<ul>
+    <li>CodeChef retrieval is now <b>re-enabled</b> and we have successfully integrated CodeChef's new API. Check your updated <a href="https://www.stopstalk.com/user/profile/%s?utm_source=newsletter&utm_medium=email&utm_campaign=important-updates&utm_content=profile-page">StopStalk profile</a> now.</li>
+    <li><a href="http://acm.timus.ru/">Timus Online Judge</a> is added to StopStalk. Head over to <a href="https://www.stopstalk.com/user/update_details?utm_source=newsletter&utm_medium=email&utm_campaign=important-updates&utm_content=update-details">Update Details page</a> to add your Timus User ID.</li>
+    <li>Spoj retrieval limit of 120 submissions is eliminated and we can now retrieve all your submissions. Please remove your Spoj handle from your profile and then re-add it to fetch all your Spoj submissions.</li>
+    <li>You can now write editorials on StopStalk and contribute to the community. Go to any problem page and click on "Editorials" to see editorials contributed by other StopStalkers or write an editorial yourself!</li>
+</ul>
+Follow our <a href="https://www.facebook.com/stopstalkcommunity">Facebook page</a> for latest updates.<br/>
+Adjust your email preferences <a href="https://www.stopstalk.com/unsubscribe?utm_source=newsletter&utm_medium=email&utm_campaign=important-updates&utm_content=unsubscribe">here</a>.<br/><br/>
 Cheers,<br/>
 Team StopStalk
 </html>
-              """ % (row.stopstalk_handle)
+              """ % (row.stopstalk_handle, row.stopstalk_handle)
     current.send_mail(to=row.email,
                       subject=subject,
                       message=message,
-                      mail_type="feature_updates")
+                      mail_type="feature_updates",
+                      bulk=True)
