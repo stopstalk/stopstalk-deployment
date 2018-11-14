79edfaf2a1931127d0133cf13861228a24b54e5d
raj454raj <raj454raj@gmail.com>
2018-11-14 10:20:49 +0530

    add todo list icon in problem pages

--- static/js/appjs/problems/trending.js
+++ static/js/appjs/problems/trending.js
@@ -23,6 +23,16 @@
         });
       }
 
+      $(document).on('mouseenter', 'tbody tr', function() {
+          var todoIcon = $(this).find('.add-to-todo-list');
+          todoIcon.show();
+      });
+
+      $(document).on('mouseleave', 'tbody tr', function() {
+          var todoIcon = $(this).find('.add-to-todo-list');
+          todoIcon.hide();
+      });
+
       $.ajax({
         url: globalTableURL,
         method: 'GET',
