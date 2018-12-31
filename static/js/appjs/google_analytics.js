369cd878c176a35bbc090edeee759d61251fba19
raj454raj <raj454raj@gmail.com>
2018-12-31 23:13:23 +0530

    add tracking for job profile nav button

--- static/js/appjs/google_analytics.js
+++ static/js/appjs/google_analytics.js
@@ -87,6 +87,7 @@
         addEventListener('.custom-user-list-name', 'Custom user name in Modal', false);
         addEventListener('.custom-user-modal-site-profile', 'Custom user Site Profile in Modal', false);
         addEventListener('#open-side-nav', 'Open Side Navbar', false);
+        addEventListener('#job-profile-cta', 'StopStalk Job profile CTA', false);
 
         addEventListener('#recent-announcements-todo-onboarding-already-using', 'Onboarding ToDo - Already using', false);
         addEventListener('#recent-announcements-todo-onboarding-will-start-using', 'Onboarding ToDo - Will start using', false);
