3e947f39341a98574fa363871708fcae4f5bc38c
raj454raj <raj454raj@gmail.com>
2019-01-01 23:19:04 +0530

    add new newsletter for job profile

--- private/scripts/send-mail.py
+++ private/scripts/send-mail.py
@@ -27,24 +27,25 @@ query = (atable.registration_key == "") & \
 rows = db(query).select(atable.stopstalk_handle, atable.email)
 
 for row in rows:
-    subject = "Recent enhancements on StopStalk"
+    subject = "StopStalk Job Profile"
     message = """
 <html>
 Hello %s,<br/><br/>
 
-We have a few major announcements to make your experience on StopStalk better - <br/>
-<ul>
-    <li>CodeChef retrieval is now <b>re-enabled</b> and we have successfully integrated CodeChef's new API. Check your updated <a href="https://www.stopstalk.com/user/profile/%s?utm_source=newsletter&utm_medium=email&utm_campaign=important-updates&utm_content=profile-page">StopStalk profile</a> now.</li>
-    <li><a href="http://acm.timus.ru/">Timus Online Judge</a> is added to StopStalk. Head over to <a href="https://www.stopstalk.com/user/update_details?utm_source=newsletter&utm_medium=email&utm_campaign=important-updates&utm_content=update-details">Update Details page</a> to add your Timus User ID.</li>
-    <li>Spoj retrieval limit of 120 submissions is eliminated and we can now retrieve all your submissions. Please remove your Spoj handle from your profile and then re-add it to fetch all your Spoj submissions.</li>
-    <li>You can now write editorials on StopStalk and contribute to the community. Go to any problem page and click on "Editorials" to see editorials contributed by other StopStalkers or write an editorial yourself!</li>
-</ul>
-Follow our <a href="https://www.facebook.com/stopstalkcommunity">Facebook page</a> for latest updates.<br/>
-Adjust your email preferences <a href="https://www.stopstalk.com/unsubscribe?utm_source=newsletter&utm_medium=email&utm_campaign=important-updates&utm_content=unsubscribe">here</a>.<br/><br/>
+We are here with an amazing announcement for you guys.<br/>
+You might be looking for Job or Internship opportunites while you are busy solving interesting problems.<br/>
+Let us know if you are interested in being contacted by companies looking to hire candidates like you.<br/><br/>
+
+Update your <a href="https://www.stopstalk.com/job_profile?utm_source=newsletter&utm_medium=email&utm_campaign=job-profile&utm_content=job-page">StopStalk Job Profile</a> now by filling the required details on the page and we will contact you if we find a match.<br/>
+We hope we can find you an apt place for your skills to grow.<br/><br/>
+
 Cheers,<br/>
-Team StopStalk
+Team StopStalk<br/><br/>
+
+--------------------------------<br/>
+Adjust your email preferences <a href="https://www.stopstalk.com/unsubscribe?utm_source=newsletter&utm_medium=email&utm_campaign=job-profile&utm_content=unsubscribe">here</a>.<br/><br/>
 </html>
-              """ % (row.stopstalk_handle, row.stopstalk_handle)
+              """ % row.stopstalk_handle
     current.send_mail(to=row.email,
                       subject=subject,
                       message=message,
