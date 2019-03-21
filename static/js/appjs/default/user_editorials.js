d885d0ea1495a0d6e4b308a332d8f716d8e43471
raj454raj <raj454raj@gmail.com>
2019-03-21 14:22:19 +0530

    add a countdown till the contest end

--- static/js/appjs/default/user_editorials.js
+++ static/js/appjs/default/user_editorials.js
@@ -1,8 +1,40 @@
 (function($) {
     "use strict";
+
+    function contestCountDownHandler() {
+      var countDownDate = 1554056999000; // March 31st 23:59:59 IST
+
+      var updateCounterInterval = setInterval(function() {
+
+        // Get todays date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Output the result in an element with id="demo"
+        document.getElementById("march-contest-countdown").innerHTML = days + " days, " + hours + " hours, "
+        + minutes + " minutes, " + seconds + " seconds left";
+
+        // If the count down is over, write some text
+        if (distance < 0) {
+          clearInterval(updateCounterInterval);
+          document.getElementById("march-contest-countdown").innerHTML = "CONTEST ENDED";
+        }
+      }, 1000);
+    }
+
     $(document).ready(function() {
       $('.modal').modal();
 
       setEditorialVoteEventListeners();
+
+      contestCountDownHandler();
     });
 })(jQuery);
