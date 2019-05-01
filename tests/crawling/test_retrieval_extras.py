a418787f475ab4b0dd69ff9f5f52683b25db52be
raj454raj <raj454raj@gmail.com>
2019-05-01 20:19:46 +0530

    [Issue #86]: Add validation for view/download submission

--- tests/crawling/test_retrieval_extras.py
+++ tests/crawling/test_retrieval_extras.py
@@ -25,6 +25,7 @@ import requests
 import sites
 
 current.environment = "test"
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36"
 
 # ==============================================================================
 class RetrievalTest:
@@ -136,6 +137,44 @@ class RetrievalTest:
         if len(failure_sites) > 0:
             raise RuntimeError(", ".join(failure_sites) + " " + "invalid handle failure")
 
+    # --------------------------------------------------------------------------
+    def test_download_submission(self):
+        import requests
+        from bs4 import BeautifulSoup
+
+        sites_with_download_functionality = ["CodeChef", "CodeForces"]
+        assertion_hash = {
+            "CodeChef": {
+                "view_link": "https://www.codechef.com/viewsolution/4530125",
+                "submission": '#incldude<stdio.h>\n#include<math.h>\n#include<string.h>\n#define g getchar_unlocked\nint prime[10000];\nint notprime[100000]={0};\nint z=0,i,j;\nint readnum()\n{\n\tint n=0;\n\tchar c=g();\n\twhile(c<\'0\'||c>\'9\')c=g();\n\twhile(c>=\'0\'&&c<=\'9\')n=10*n+c-\'0\',c=g();\n\treturn n;\n}\ninline void fastWrite(int a)\n{\n\tchar snum[20];\n\tint i=0;\n\tdo\n\t{\n\t\tsnum[i++]=a+48;\n\t\ta=a/10;\n\t}while(a!=0);\n\ti=i-1;\n\twhile(i>=0)\n\t\tputchar_unlocked(snum[i--]);\n\tputchar_unlocked(\'\\n\');\n}\nvoid seive()\n{\n\tnotprime[0]=1;\n\tnotprime[1]=1;\n\tfor(i=2;i<100000;i++)\n\t{\n\t\tif(notprime[i]==0)\n\t\t{\n\t\t\tprime[z++]=i;\n\t\t\tfor(j=i+i;j<100000;j+=i)\n\t\t\t\tnotprime[j]=1;\n\t\t}\n\t}\n\t \n}\nint main()\n{\n\tseive();\n\t/* for(i=0;i<1000;i++)\n\t   printf("%d ",prime[i]);\n\t   */\tint m,n,t,i,sq,flag;\n\t//scanf("%d",&t);\n\tt=readnum();\n\twhile(t--)\n\t{\n\t\t//scanf("%d%d",&m,&n);\n\t\tm=readnum();\n\t\tn=readnum();\n\t\tif(m<=2)\n\t\t{\n\t\t\tprintf("2\\n");\n\t\t\tm=3;\n\t\t}\n\t\telse if(m%2==0)\n\t\t\tm=m+1;\n\t\tfor(i=m;i<=n;i+=2)\n\t\t{\n\t\t\tflag=0;\n\t\t\tsq=sqrt(i);\n\t\t\tfor(j=1;prime[j]<=sq;j++)\n\t\t\t{\n\t\t\t\tif(i%prime[j]==0)\n\t\t\t\t{\n\t\t\t\t\tflag=1;\n\t\t\t\t\tbreak;\n\t\t\t\t}\n\t\t\t}\n\t\t\tif(flag==0)\n\t\t\t\tfastWrite(i);\n\t\t}\n\t}\n\treturn 0;\n}\n'
+            },
+            "CodeForces": {
+                "view_link": "http://www.codeforces.com/contest/454/submission/7375767",
+                "submission": '#include<stdio.h>\nint main()\n{\n\tint n,i,j,k;\n\tscanf("%d",&n);\n\tint h=n/2+1;\n\tfor(i=0;i<h;i++)\n\t{\n\t\tfor(k=0;k<n/2-i;k++)\n\t\t\tprintf("*");\n\t\tfor(j=0;j<2*i+1;j++)\n\t\t\tprintf("D");\n\t\tfor(j=n/2+i+1;j<n;j++)\n\t\t\tprintf("*");\n\t\tprintf("\\n");\n\t}\n\tfor(i=0;i<n/2;i++)\n\t{\n\t\tfor(k=0;k<=i;k++)\n\t\t        printf("*");\n\t\tfor(j=n-2*i;j>=3;j--)\n\t\t\tprintf("D");\n\t\tfor(j=0;j<=i;j++)\n\t\t\tprintf("*");\n\t\tprintf("\\n");\n\t}\n\treturn 0;\n}\n'
+            }
+        }
+
+        # Copied from https://github.com/stopstalk/stopstalk-deployment/blob/ac78cfcba4874fb1a5a6ac73112933fb0faa89c3/controllers/default.py#L1462
+        def _response_handler(download_url, response):
+            return BeautifulSoup(response.text, "lxml").find("pre").text
+
+        def _retrieve_codechef_submission(view_link):
+            problem_id = view_link.strip("/").split("/")[-1]
+            download_url = "https://www.codechef.com/viewplaintext/" + \
+                           str(problem_id)
+            response = requests.get(download_url,
+                                    headers={"User-Agent": user_agent})
+            return _response_handler(download_url, response)
+
+        def _retrieve_codeforces_submission(view_link):
+            response = requests.get(view_link)
+            return _response_handler(view_link, response)
+
+        for site in sites_with_download_functionality:
+            method_name = locals()["_retrieve_%s_submission" % site.lower()]
+            if method_name(assertion_hash[site]["view_link"]) != assertion_hash[site]["submission"]:
+                raise RuntimeError(site + " download submission failed")
+
 # ------------------------------------------------------------------------------
 def test_retrieval(retrieval_object, method_name):
     error_message = ""
@@ -152,7 +191,10 @@ def test_retrieval(retrieval_object, method_name):
 rt = RetrievalTest()
 pushover_message = ""
 
-for method_name in ["test_tag_retrieval", "test_editorial_retrieval", "test_invalid_handle"]:
+for method_name in ["test_tag_retrieval",
+                    "test_editorial_retrieval",
+                    "test_invalid_handle",
+                    "test_download_submission"]:
     res = test_retrieval(rt, method_name)
     if res != "Success":
         pushover_message += res + "\n"
