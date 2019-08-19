a70cc888f18494e7ee3738407a59578012c310c3
raj454raj <raj454raj@gmail.com>
2019-08-20 00:00:10 +0530

    minor tweaks

--- exit_code_handler.sh
+++ exit_code_handler.sh
@@ -5,5 +5,6 @@ command_to_execute=$1
 eval $command_to_execute
 exit_code=`echo $?`
 if [[ $exit_code -ne 0 ]]; then
-  mail -s "[`hostname`] - Command failed: $command_to_execute, Exit code: " raj454raj@gmail.com <<< "Nothing"
+  subject="[`hostname`] - Command failed: $command_to_execute, Exit code: $exit_code"
+  mail -s "$subject" raj454raj@gmail.com <<< "-"
 fi
