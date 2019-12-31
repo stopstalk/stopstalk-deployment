eda76a7266873c859624acd9b360c700cf0cc4a0
raj454raj <raj454raj@gmail.com>
2020-01-01 01:43:18 +0530

    Happy new year - 2020 ;)

--- private/scripts/extras/resetDB.py
+++ private/scripts/extras/resetDB.py
@@ -1,5 +1,5 @@
 """
-    Copyright (c) 2015-2019 Raj Patel(raj454raj@gmail.com), StopStalk
+    Copyright (c) 2015-2020 Raj Patel(raj454raj@gmail.com), StopStalk
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
