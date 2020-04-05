09a60d05b1ac71b259ef7f0d50b389048a24368c
raj454raj <raj454raj@gmail.com>
2020-04-05 16:13:39 +0530

    add healthcheck for status update

--- controllers/default.py
+++ controllers/default.py
@@ -29,6 +29,10 @@ import json
 
 user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36"
 
+# ------------------------------------------------------------------------------
+def status_check():
+    return "up and running..."
+
 # ----------------------------------------------------------------------------
 def handle_error():
     """
