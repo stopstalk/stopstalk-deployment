4a12991dec70f4cf393a3357e6c39a81dad641ca
raj454raj <raj454raj@gmail.com>
2020-09-27 16:01:12 +0530

    [Issue #369]: add utm params to institute_user emails

--- private/scripts/flush-institute-user.py
+++ private/scripts/flush-institute-user.py
@@ -45,7 +45,14 @@ rows = db(atable.id.belongs(all_user_ids)).select(atable.id,
 for row in rows:
     id_to_record[row.id] = row
 
+base_utm_params = {
+  "utm_source": "email",
+  "utm_medium": "email",
+  "utm_campaign": "institute_user"
+}
+
 def send_message(to_record, from_records):
+    utm_params = base_utm_params.copy()
     names = [str(A(x.first_name + " " + x.last_name,
                    _href=URL("user",
                              "profile",
@@ -53,39 +60,59 @@ def send_message(to_record, from_records):
                              scheme="https",
                              host="www.stopstalk.com",
                              extension=False))) for x in from_records]
+    names = []
+    for x in from_records:
+        utm_params.update({
+            "utm_term": x.stopstalk_handle,
+            "utm_content": "profile_link"
+        })
+        names.append(str(A(x.first_name + " " + x.last_name,
+                           _href=URL("user",
+                                     "profile",
+                                     args=x.stopstalk_handle,
+                                     scheme="https",
+                                     host="www.stopstalk.com",
+                                     vars=utm_params,
+                                     extension=False))))
 
     has_have = ""
     if len(from_records) == 1:
-        subject = "Someone registered from your Institute"
+        subject = "Someone registered from your Institute!"
         name_string = names[0]
         address_string = "him / her"
         has_have = "has"
     else:
-        subject = "A few people registered from your Institute"
+        subject = "A few people registered from your Institute!"
         name_string = ", ".join(names[:-1]) + " and " + names[-1]
         address_string = "them"
         has_have = "have"
+
+    utm_params.update({
+        "utm_term": "unsubscribe",
+        "utm_content": "unsubscribe_link"
+    })
 
     message = """
 <html>
 Hello %s,<br/>
 <br/>
 %s from your Institute %s just joined StopStalk.<br/>
-Add %s as your friend now for better experience on StopStalk.<br/>
+Add %s as your friend now for better learning experience on StopStalk.<br/>
 <br/>
-Adjust your email preferences <a href="%s">here</a><br/>
+Adjust your email preferences <a href="%s">here</a><br/>.
 <br/>
 Cheers,<br />
 Team StopStalk
 </html>
 """ % (to_record.stopstalk_handle,
-       name_string,
+       name_string.replace("&amp;", "&"),
        has_have,
        address_string,
        URL("default",
            "unsubscribe",
            scheme="https",
            host="www.stopstalk.com",
+           vars=utm_params,
            extension=False))
 
     current.send_mail(to=to_record.email,
