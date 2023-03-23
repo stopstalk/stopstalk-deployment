ee18287f89c1a960581b65b1d4a7451e5f37dce9
raj454raj <raj454raj@gmail.com>
2023-03-23 14:12:06 +0530

    recheck handle query with id instead of handle

--- private/scripts/recheck-invalid-handles.py
+++ private/scripts/recheck-invalid-handles.py
@@ -97,10 +97,12 @@ if __name__ == "__main__":
                 if "user_id" in row_obj:
                     # Custom user
                     db(nrtable.custom_user_id == row_obj.id).update(**{row.site.lower() + "_delay": 0})
+                    final_delete_query |= ((stable.site == row.site) & \
+                                           (stable.custom_user_id == row_obj.id))
                 else:
                     db(nrtable.user_id == row_obj.id).update(**{row.site.lower() + "_delay": 0})
-                final_delete_query |= ((stable.site == row.site) & \
-                                       (stable.stopstalk_handle == row_obj.stopstalk_handle))
+                    final_delete_query |= ((stable.site == row.site) & \
+                                           (stable.user_id == row_obj.id))
                 del update_dict[row.site.lower() + "_lr"]
             row.delete_record()
         if cnt >= 10:
